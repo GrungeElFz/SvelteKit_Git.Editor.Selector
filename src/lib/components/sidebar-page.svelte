84c855c6e71b114f025f90388ddcb2ba0ec58baf
GrungeElFz <elf0114_@hotmail.com>
2024-11-12 13:09:34 -0600

    Modified:   Made `<SidebarPage />` to be a reusable component (src/lib/components/sidebar-page.svelte)             Moved a Theme Switcher from the main page to `<SidebarPage />`
    Modified:   Passed `<SidebarPage />` component into the main page (src/routes/+page.svelte)

--- src/lib/components/sidebar-page.svelte
+++ src/lib/components/sidebar-page.svelte
@@ -3,6 +3,13 @@
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	// Theme Switcher
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { resetMode, setMode } from 'mode-watcher';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
 </script>
 
 <Sidebar.Provider>
@@ -26,14 +33,29 @@
 					</Breadcrumb.List>
 				</Breadcrumb.Root>
 			</div>
+
+			<!-- Theme Switcher -->
+			<div class="ml-auto mr-5">
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
+						<Sun
+							class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						/>
+						<Moon
+							class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						/>
+						<span class="sr-only">Toggle theme</span>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Item onclick={() => setMode('light')}>Light</DropdownMenu.Item>
+						<DropdownMenu.Item onclick={() => setMode('dark')}>Dark</DropdownMenu.Item>
+						<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-				<div class="aspect-video rounded-xl bg-muted/50"></div>
-				<div class="aspect-video rounded-xl bg-muted/50"></div>
-				<div class="aspect-video rounded-xl bg-muted/50"></div>
-			</div>
-			<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
+			<slot />
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>

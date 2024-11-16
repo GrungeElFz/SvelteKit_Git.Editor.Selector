73c9f826ce4880361f90e38b882554c8cf4bdfa7
GrungeElFz <elf0114_@hotmail.com>
2024-11-15 22:49:32 -0600

    Modified:   Inserted `<Introduction />` to the main page (src/routes/+page.svelte)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import SidebarPage from '$lib/components/sidebar-page.svelte';
+	import Introduction from '@docs/Introduction.svx';
 </script>
 
-<SidebarPage />
+<SidebarPage>
+	<Introduction />
+</SidebarPage>
